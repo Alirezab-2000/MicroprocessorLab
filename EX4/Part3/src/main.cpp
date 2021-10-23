6fc4c4349b37f5695495644c8d54080c1a93314f
Alirezab-2000 <alireza.binesh.2000@gmail.com>
2021-10-23 15:37:25 +0330

    FEAT: EX4 Part3

--- EX4/Part3/src/main.cpp
+++ EX4/Part3/src/main.cpp
@@ -6,16 +6,18 @@
 
 unsigned int overflows_count = 0;
 
-unsigned int second = 0;
-unsigned int minute = 0;
-unsigned int hour = 0;
+signed int second = 0;
+signed int minute = 0;
+signed int hour = 0;
+
+bool alarm_timer = false;
 
 char time[10] = "";
 
 void show_time()
 {
     LCD_cmd(0x01); //clear lcd
-    sprintf(time, "time: %d",second);
+    sprintf(time, "%d:%d:%d", hour, minute, second);
     LCD_Write_String(time);
 }
 void decrease_second()
@@ -24,52 +26,64 @@ void decrease_second()
 
     if (second == -1)
     {
+        minute--;
+        second = 59;
+    }
+    if (minute == -1)
+    {
+        hour--;
+        minute = 59;
+    }
+    if (hour == -1)
+    {
+        alarm_timer = false;
         PORTA = 0x01;
 
         LCD_cmd(0x01);
         LCD_Write_String("....END....");
     }
-}
-void change_timer_status(bool isStop)
-{
-    if (isStop)
-    {
-        TCCR1B &= ~(1 << CS12);
-        TCCR1B &= ~(1 << CS11);
-        TCCR1B &= ~(1 << CS10);
-    }
     else
     {
-        TCCR1B &= ~(0 << CS10);
+        show_time();
     }
 }
-
-
-
-
 
 ISR(INT0_vect) //External interrupt_zero ISR
 {
-    if(TCCR1B && 0x01 == 0){
-        change_timer_status(false);
-    }else{
-        change_timer_status(true);
+    if ((PINC & 0x01) != 0)
+    {
+        alarm_timer = !alarm_timer;
     }
-    
-}
-ISR(INT1_vect) //External interrupt_one ISR
-{
-    second = second + 1;
+    else if ((PINC & 0x02) != 0)
+    {
+        second = second + 1;
 
-    show_time();
+        show_time();
+    }
+    else if ((PINC & 0x04) != 0)
+    {
+        minute = minute + 1;
+
+        show_time();
+    }
+    else if ((PINC & 0x08) != 0)
+    {
+        hour = hour + 1;
+
+        show_time();
+    }
 }
+
 ISR(TIMER1_OVF_vect)
 {
-    overflows_count++;
-
-    if (overflows_count % 15 == 0)
+    if (alarm_timer)
     {
-        show_time();
+        overflows_count++;
+
+        if (overflows_count % 15 == 0)
+        {
+            decrease_second();
+        }
     }
 }
 int main()
@@ -83,14 +97,13 @@ int main()
     init_LCD();    // initialize LCD
     LCD_cmd(0x0C); // display on, cursor off
 
-    show_time();   // show the 0 time
+    show_time(); // show the 0 time
 
     TIMSK = (1 << TOIE1); // enable timer2 overflow
-    // TCCR1B = (1 << CS10); // set timer2 prescaler to clock (no prescaling)
+    TCCR1B = (1 << CS10); // set timer2 prescaler to clock (no prescaling)
 
-    GICR |= (1 << INT0) | (1 << INT1);    //Enable External Interrupts INT0 and INT1
+    GICR |= 1 << INT0;                    // Enable External Interrupts INT0 and INT1
     MCUCR |= (1 << ISC00) | (1 << ISC01); // set INT0 to trigger the intrupt on rasing edge
-    MCUCR |= (1 << ISC11) | (1 << ISC10); // set INT1 to trigger the intrupt on rasing edge
 
     sei(); // enable all intrupts
 
