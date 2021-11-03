7609fe7420ca97675682020a20300aa4f8fb4073
Alirezab-2000 <alireza.binesh.2000@gmail.com>
2021-11-03 10:43:12 +0330

    FIX: some changes in comments in code

--- EX6/part1/src/main.cpp
+++ EX6/part1/src/main.cpp
@@ -11,21 +11,13 @@ char temp_fahrenheit_str[20] = "";
 uint16_t adc_read(uint8_t ch)
 {
     // select the corresponding channel 0~7
-    // ANDing with '7' will always keep the value
-    // of 'ch' between 0 and 7
     ch &= 0b00000111;            // AND operation with 7
-    ADMUX = (ADMUX & 0xF8) | ch; // clears the bottom 3 bits before ORing
-
-    // start single conversion
+    ADMUX = (ADMUX & 0xF8) | ch; 
     // write '1' to ADSC
     ADCSRA |= (1 << ADSC);
-
     // wait for conversion to complete
-    // ADSC becomes '0' again
-    // till then, run loop continuously
     while (ADCSRA & (1 << ADSC))
         ;
-
     return (ADC);
 }
 
